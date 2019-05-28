9998080674225fd62c886c8f4f24f500e04d4e40
varol <murat.varol@kgteknoloji.com>
2019-05-28 16:53:38 +0300

    GlideModule to generate Glide

---
+++ app/src/main/java/com/varol/movier/util/GlideAppModule.kt
@@ -0,0 +1,8 @@
+package com.varol.movier.util
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+
+@GlideModule
+class GlideAppModule : AppGlideModule()
