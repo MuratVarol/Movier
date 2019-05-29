ec5b7fa9311fe57d4f8b81e5c8ae89b0cdb913a4
varol <murat.varol@kgteknoloji.com>
2019-05-29 14:20:44 +0300

    description comment line for dropBreadcrumb extension

--- app/src/main/java/com/varol/movier/extension/RxExtensions.kt
+++ app/src/main/java/com/varol/movier/extension/RxExtensions.kt
@@ -3,6 +3,11 @@ package com.varol.movier.extension
 import io.reactivex.Single
 import io.reactivex.exceptions.CompositeException
 
+
+/**
+ * RxJava hides inner exception on console;
+ * this extension using for better error logging for child observers
+ */
 fun <T> Single<T>.dropBreadcrumb(): Single<T> {
     val breadcrumb = BreadcrumbException()
     return this.onErrorResumeNext { error: Throwable ->
