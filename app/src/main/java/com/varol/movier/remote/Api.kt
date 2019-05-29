00de0921bf36d218cc3cf6e34df8c65d53cca8b6
varol <murat.varol@kgteknoloji.com>
2019-05-29 15:46:43 +0300

    getmovies by type method added

--- app/src/main/java/com/varol/movier/remote/Api.kt
+++ app/src/main/java/com/varol/movier/remote/Api.kt
@@ -1,6 +1,21 @@
 package com.varol.movier.remote
 
+import com.varol.movier.entity.MoviesModel
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.http.Url
+
 interface Api {
+
+    @GET
+    fun getMovies(
+        @Url url: String,
+        @Query(value = "api_key") apiKey: String,
+        @Query(value = "page") page: Int,
+        @Query(value = "language") language: String,
+        @Query(value = "region") region: String
+    ): Single<BaseMoviesResponse<MutableList<MoviesModel>>>
 
 
 }
