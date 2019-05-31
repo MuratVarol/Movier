bdcf439907d8b85ca948704f4e08754acddd47dd
varol <murat.varol@kgteknoloji.com>
2019-05-31 14:38:10 +0300

    MovieDetailVM added to module

--- app/src/main/java/com/varol/movier/di/ViewModelModule.kt
+++ app/src/main/java/com/varol/movier/di/ViewModelModule.kt
@@ -1,6 +1,7 @@
 package com.varol.movier.di
 
 import com.varol.movier.viewmodel.MainVM
+import com.varol.movier.viewmodel.MovieDetailVM
 import com.varol.movier.viewmodel.MoviesVM
 import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.Module
@@ -9,4 +10,5 @@ import org.koin.dsl.module.module
 val viewModelModule: Module = module {
     viewModel { MainVM() }
     viewModel { MoviesVM(get()) }
+    viewModel { MovieDetailVM() }
 }
