a2e0dd7445e338a682aaa6c3470ecf1bf6bc9705
varol <murat.varol@kgteknoloji.com>
2019-05-31 14:31:08 +0300

    link changed to discover

--- app/src/main/java/com/varol/movier/remote/Api.kt
+++ app/src/main/java/com/varol/movier/remote/Api.kt
@@ -2,15 +2,16 @@ package com.varol.movier.remote
 
 import com.varol.movier.model.MoviesModel
 import io.reactivex.Single
-import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 import retrofit2.http.Url
 
 interface Api {
 
-    @GET
+    @POST
     fun getMovies(
         @Url url: String,
+        @Query(value = "sort_by") sort_by: String,
         @Query(value = "api_key") apiKey: String,
         @Query(value = "page") page: Int,
         @Query(value = "language") language: String,
